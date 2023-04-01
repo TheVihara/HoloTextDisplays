74f8376c3fa1248e5790139994bd6daa20406c29
Vihara <gorenjecalen@gmail.com>
2023-04-01 02:01:21 +0200

    init commit

---
+++ src/main/java/me/gorenjec/holotextdisplays/HoloTextDisplays.java
@@ -0,0 +1,16 @@
+package me.gorenjec.holotextdisplays;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HoloTextDisplays extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
